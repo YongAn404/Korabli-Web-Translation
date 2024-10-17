27af659510226d80be1478a58df301f1e6769ed3
永安404 <101850798+YongAn404@users.noreply.github.com>
2024-10-17 10:57:07 +0800

    修复

--- Korabli-Web-Translation.js
+++ Korabli-Web-Translation.js
@@ -7,17 +7,15 @@
 // @match        https://korabli.su/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @require      https://scriptcat.org/lib/513/2.0.0/ElementGetter.js
-// @connect      github.com
+// @connect      raw.githubusercontent.com
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
 (function() {
     GM_xmlhttpRequest({
-        url:"https://github.com/YongAn404/Korabli-Web-Translation/blob/dev/i18n/zh-cn/news/content.json",
-        method :"POST",
-        headers: {
-            "Content-type": "application/x-www-form-urlencoded"
-        },
+        url:"https://raw.githubusercontent.com/YongAn404/Korabli-Web-Translation/refs/heads/dev/i18n/zh-cn/news/content.json",
+        method :"GET",
+        dataType: 'json',
         onload:function(xhr){
             let json = JSON.parse(xhr.responseText);
             elmGetter.each('.news-card', document, reply => {
@@ -43,11 +41,9 @@
     });
 
     GM_xmlhttpRequest({
-        url:"https://github.com/YongAn404/Korabli-Web-Translation/blob/dev/i18n/zh-cn/news/label.json",
-        method :"POST",
-        headers: {
-            "Content-type": "application/x-www-form-urlencoded"
-        },
+        url:"https://raw.githubusercontent.com/YongAn404/Korabli-Web-Translation/refs/heads/dev/i18n/zh-cn/news/label.json",
+        method :"GET",
+        dataType: 'json',
         onload:function(xhr){
             let json = JSON.parse(xhr.responseText);
             elmGetter.each('.news-card', document, reply => {
