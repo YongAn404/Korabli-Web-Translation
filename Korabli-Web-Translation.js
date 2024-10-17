36adf5ccfc21250f9cb1ba67e86105f220da463f
永安404 <101850798+YongAn404@users.noreply.github.com>
2024-10-17 15:12:30 +0800

    U2

--- Korabli-Web-Translation.js
+++ Korabli-Web-Translation.js
@@ -8,10 +8,27 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
 // @require      https://scriptcat.org/lib/513/2.0.0/ElementGetter.js
 // @connect      raw.githubusercontent.com
+// @grant        GM_getResourceText
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
 (function() {
+
+    let script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.src = "https://raw.githubusercontent.com/YongAn404/Korabli-Web-Translation/refs/heads/dev/i18n/zh-cn/korabli/i18n.json";
+    document.documentElement.appendChild(script);
+
+    script = document.createElement('script');
+    script.setAttribute('type', 'text/javascript');
+    script.src = "https://raw.githubusercontent.com/YongAn404/Korabli-Web-Translation/refs/heads/dev/i18n/zh-cn/korabli/i10n.json";
+    document.documentElement.appendChild(script);
+
+    script = document.createElement('link');
+    script.setAttribute('rel', 'preload');
+    script.setAttribute('as', 'script');
+    script.href = "https://raw.githubusercontent.com/YongAn404/Korabli-Web-Translation/refs/heads/dev/i18n/zh-cn/korabli/i10n.json";
+    document.documentElement.appendChild(script);
 
     // categories
 
@@ -25,28 +42,16 @@
                 //处理标签
                 const NCTS = reply.querySelectorAll('.news-card_title');
                 for (const key in NCTS) {
-                    const NCT = NCTS[key];
-                    let item = json[NCT.innerHTML];
-                    if(typeof item != "undefined"){
-                        NCT.innerHTML = item["title"];
-                    }
+                    setJsonToHtml(json,NCTS[key],"title")
                 }
             });
 
             elmGetter.each('.main', document.querySelector('.news-detail__content'), reply => {
-                const placeholder = reply.querySelector('placeholder');
-                const item = json[placeholder.innerHTML];
-                if(typeof item != "undefined"){
-                    placeholder.innerHTML=item["title"];
-                }
+                setJsonToHtml(json,reply.querySelector('.placeholder'),"title")
             });
 
             elmGetter.each('.news-header', document, reply => {
-                const title = reply.querySelector('.title');
-                const item = json[title.innerHTML];
-                if(typeof item != "undefined"){
-                    title.innerHTML=item["title"];
-                }
+                setJsonToHtml(json,reply.querySelector('.title'),"title")
             });
         }
     });
@@ -61,11 +66,7 @@
                 //处理勋章
                 const NCLS = reply.querySelectorAll('.news-card_label');
                 for (const key in NCLS) {
-                    const NCL = NCLS[key];
-                    let text = json[NCL.innerHTML];
-                    if(typeof text != "undefined"){
-                        NCL.innerHTML = text;
-                    }
+                    setJsonToHtml(json,NCLS[key])
                 }
             });
         }
@@ -79,22 +80,32 @@
         onload:function(xhr){
             let json = JSON.parse(xhr.responseText);
             elmGetter.each('#footer', document, reply => {
-                const classData = json["class"]
-                for (const key in classData) {
-                    const classText = classData[key];
-                    const classHTML = reply.querySelector(`.${key}`);
-                    classHTML.innerHTML = classText
-                }
+                setClassJsonToHtml(json,reply);
 
                 const as = reply.querySelectorAll('a');
                 for (const key in as) {
-                    const a = as[key];
-                    let text = json[a.innerHTML];
-                    if(typeof text != "undefined"){
-                        a.innerHTML = text;
-                    }
+                    setJsonToHtml(json,as[key]);
+                }
+                const h4s = reply.querySelectorAll('h4');
+                for (const key in h4s) {
+                    setJsonToHtml(json,h4s[key]);
                 }
             });
         }
     });
 })();
+
+function setClassJsonToHtml(json,reply){
+    const classData = json["class"]
+    for (const key in classData) {
+        const classText = classData[key];
+        const classHTML = reply.querySelector(`.${key}`);
+        classHTML.innerHTML = classText
+    }
+}
+function setJsonToHtml(json,dom,key=null){
+    let item = json[dom.innerHTML];
+    if(typeof item != "undefined"){
+        dom.innerHTML = key==null?item:item[key];
+    }
+}
